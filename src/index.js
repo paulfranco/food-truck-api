428f3c5b8aea210cea1144d834ffe06a0dbec918
Paul Franco <paulfranco@me.com>
2018-05-22 05:55:19 -0700

    Added POST Route

--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ import config from './config';
 import routes from './routes';
 
 let app = express();
-app.server = http:createServer(app);
+app.server = http.createServer(app);
 
 // middleware
-
+// parse application/json
+app.use(bp.json({
+    limit: config.bodyLimit
+}))
 // passport config
 
 // api route v1
 app.use('/v1', routes);
 
 app.server.listen(config.port);
-console.log('Started on port: ${app.server.address().port}');
+console.log('Started on port ${app.server.address().port}');
 
 export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import config from '../config';
-import middleware from '...middleware';
+import middleware from '../middleware';
 import initializeDb from '../db';
+import restaurant from '../controller/restaurant';
 
 let router = express();
 
@@ -10,6 +11,7 @@ initializeDb(db => {
 //internal middleware
     router.use(middleware({ config, db }));
 //api routes v1(/v1)
+    router.use('/restaurant', restaurant({ config, db }));
 
 });
 
