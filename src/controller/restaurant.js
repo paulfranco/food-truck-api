0e30707ffd6644edd403b03e2798ed242a9c3200
Paul Franco <paulfranco@me.com>
2018-05-22 06:17:24 -0700

    Added GET Routes

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -4,8 +4,8 @@ import Restaurant from '../model/restaurant';
 
 export default({ config, db}) => {
     let api = Router();
-
-    // 'v1/restaurant/add'
+    // CRUD - Create, Read, Update, Delete
+    // 'v1/restaurant/add' - Create
     api.post('/add', (req, res) => {
         let newRest = new Restaurant();
         newRest.name = req.body.name;
@@ -15,6 +15,26 @@ export default({ config, db}) => {
                 res.setEncoding(err);
             } else {
                 res.json({message: 'Restaurant saved successfully'});
+            }
+        });
+    });
+    // 'v1/restaurant' - Read
+    api.get('/', (req, res) => {
+        Restaurant.find({}, (err, restaurants) => { // Mongo find everything when empty
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(restaurants);
+            }
+        }); 
+    });
+    // 'v1/restaurant/:id' - Read 1
+    api.get('/:id', (req, res) => {
+        Restaurant.findById(req.params.id, (err, restaurant) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(restaurant);
             }
         });
     });
