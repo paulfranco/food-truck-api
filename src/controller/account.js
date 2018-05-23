94d492a4df05d8523584ac5e0b862d579614abaf
Paul Franco <paulfranco@me.com>
2018-05-23 02:27:56 -0700

    Added Account model

---
+++ src/controller/account.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import { Router } from 'express';
+import Account from '../model/account';
+import bp from 'body-parser';
+import passport from 'passport';
+import config from '../config';
+
+import { generateAccessToken, respond, authenticate } from '../middleware/authMiddleware';
+
+export default ({ config, db }) => {
+    let api = Router();
+
+    return api;
+};
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import http from 'http';
 import express from 'express';
 import bp from 'body-parser';
 import mongoose from 'mongoose';
+import passport from 'passport';
+
+const LocalStrategy = require('passport-local').Strategy;
 
 import config from './config';
 import routes from './routes';
@@ -15,6 +18,17 @@ app.use(bp.json({
     limit: config.bodyLimit
 }))
 // passport config
+app.use(passport.initialize());
+let Account = require('./model/account');
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'  
+},
+Account.authenticate()
+));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 
 // api route v1
 app.use('/v1', routes);
