51d94e89177461a92566348b6de445a99709f1ce
Paul Franco <paulfranco@me.com>
2018-05-23 03:24:15 -0700

    Added Routes and Authentication for Account Model

--- src/controller/account.js
+++ src/controller/account.js
@@ -10,5 +10,38 @@ import { generateAccessToken, respond, authenticate } from '../middleware/authMi
 export default ({ config, db }) => {
     let api = Router();
 
+    // '/v1/account
+    api.post('/register', (req, res) => {
+        Account.register(new Account({ username: req.body.email}), req.body.password, function(err, account) {
+            if (err) {
+                res.send(err);
+            } else {
+                passport.authenticate(
+                    'local', {
+                        session: false
+                    })(req,res, () => {
+                        res.status(200).send("Successfuly created a new account");
+                    });
+                }
+        });
+    });
+    // '/v1/account/login'
+    api.post('/login', passport.authenticate(
+        'local', {
+            session: false,
+            scope: []
+        }), generateAccessToken, respond);
+    
+
+    // '/v1/account/logout'
+    api.get('/logout', authenticate, (req, res) => {
+        res.logout();
+        res.status(200).send("Successfully logged out");
+    });
+
+    // '/v1/account/me'
+    api.get('/me', authenticate, (req, res) => {
+        res.status(200).json(req.user);
+    });
     return api;
 };
